db6a9c89dea49487ae56573b015e68805fa034b5
Adelina <adelhetman@gmail.com>
2022-09-16 16:50:32 +0300

    created a react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ReactLoading from "react-loading";
+import "./App.css";
 
-function App() {
+
+export default function App() {
+  let [query, setQuery] = useState("");
+  let [weather, setWeather] = useState({});
+  let [city, setCity] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  function showWeather(response) {
+    setQuery(true);
+
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+}  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "1dad91bc92f6c69698e1aad50d0a7304";
+    let units = `metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity (event.target.value)
+  }
+  let searchForm = (
+    <div className="Weather">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
+
+  if (query) {
+    return (
+      <div>
+        {searchForm}
+        <ul>
+          <li>Temperature: {weather.temperature} ℃</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity} %</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {searchForm}
+        <ReactLoading type={"Bubbles"} color={"pink"} height={80} width={80} />
+      </div>
+    );
+  }
+</div>
 }
 
-export default App;
