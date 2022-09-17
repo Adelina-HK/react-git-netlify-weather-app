db993cd53f176ff9856ce32ea4af88464fb603d6
Adelina <adelhetman@gmail.com>
2022-09-17 13:52:09 +0300

    added a link to GitHub

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <a href="https://github.com/Adelina-HK/react-git-netlify-weather-app">
+      See my code on my GitHub{" "}
+    </a>
   </React.StrictMode>
 );
 
